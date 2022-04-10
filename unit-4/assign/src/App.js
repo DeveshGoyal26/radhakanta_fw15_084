3ee451d8af621e73f64a377c180ac09c13a2208b
RadhakantaChhatria <9999radhakant@gmail.com>
2022-04-10 22:27:55 +0530

    problem1

---
+++ unit-4/assign/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Menu1, Menu2 } from './sprint-1/day-1/assignments/Assign1/Menu';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Menu1/>
+     <Menu2/>
+    </div>
+  );
+}
+
+export default App;
