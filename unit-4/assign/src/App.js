5efc11acf393c45024f543f9880ef71e6d58a661
RadhakantaChhatria <9999radhakant@gmail.com>
2022-04-17 21:11:14 +0530

    done

--- unit-4/assign/src/App.js
+++ unit-4/assign/src/App.js
@@ -1,14 +1,43 @@
 
 import './App.css';
 import { Menu1, Menu2 } from './sprint-1/day-1/assignments/Assign1/Menu';
+import ContactUs from './sprint-1/day-1/assignments/Assign2/ContactUs';
+import Download from './sprint-1/day-1/assignments/Assign2/Download';
+import Help from './sprint-1/day-1/assignments/Assign2/Help';
+import Home from './sprint-1/day-1/assignments/Assign2/Home';
+import JoinUs from './sprint-1/day-1/assignments/Assign2/JoinUs';
+import Login from './sprint-1/day-1/assignments/Assign2/Login';
+import Search from './sprint-1/day-1/assignments/Assign2/Search';
+import Settings from './sprint-1/day-1/assignments/Assign2/Settings';
+import Card_create from './sprint-1/day-3/assignments/Prob1/CreateCard';
+import GiftCard from './sprint-1/day-3/assignments/Prob1/Gift_card';
+
 
 function App() {
   return (
+    <>
     <div className="App">
      
-     <Menu1/>
+     {/* <Menu1/>
      <Menu2/>
     </div>
+    <div className="buttons">
+      <JoinUs/>
+      <Settings/>
+      <Login/>
+      <ContactUs/>
+      <Search/>
+      <Help/>
+      <Home/>
+      <Download/> */}
+
+      {/* Day 3 */}
+
+      <div><Card_create/></div>
+     <div> <GiftCard/></div>
+
+      </div> 
+    </>
   );
 }
 
